2360d45e9d803f9a016bf6289336891951876194
n.badiullin <n.badiullin@transtelematica.ru>
2019-04-06 18:30:10 +0400

    add landing layout

---
+++ client/src/layouts/Landing/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import block from "bem-cn";
+
+import "./index.scss";
+import Button from "../../components/Button/Button";
+
+const b = block("heading");
+const b2 = block("para");
+
+const Landing = () => (
+  <div className="landing">
+    <div className="container">
+      <div className="row">
+        <div className="col-xs-9 col-md-6 col-lg-6">
+          <h1 className={b()}>Узнать за 30 секунд</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xs-9 col-md-6 col-lg-6">
+          <p className={b2()}>
+            Простая игра, вдохновленная «Афиша. Видео».
+            Теперь поиграть могут не только рэперы, но и ты
+          </p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xs-4 col-md-4 col-lg-4">
+          <a href="/game">
+            <Button artist="Сыграть" theme="landing"/>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Landing;
