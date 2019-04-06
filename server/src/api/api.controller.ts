e64c9433ea031298eeab87f61a9079297d81f020
Dmitry Dergunov <dergunovdv@yandex-team.ru>
2019-04-06 12:38:48 +0300

    make API for fethc tracks

--- server/src/api/api.controller.ts
+++ server/src/api/api.controller.ts
@@ -8,17 +8,22 @@ export class ApiController {
 
   @Get('chart')
   async getChart(): Promise<any> {
-    return await this.apiService.getChart().then(res => res.data);
+    return await this.apiService.getChart();
   }
 
   @Get('playlist')
   async getPlaylist(@Param() id: number): Promise<any> {
-    return await this.apiService.getPlaylist(id).then(res => res.data);
+    return await this.apiService.getPlaylist();
   }
 
-  @Get('track')
+  @Get('playlist/:id')
+  async getPlaylistById(@Param() id: number): Promise<any> {
+    return await this.apiService.getPlaylist(id);
+  }
+
+  @Get('track/:id')
   async getTrack(@Param() id: number): Promise<any> {
-    return await this.apiService.getTrack(id).then(res => res.data);
+    return await this.apiService.getTrack(id);
   }
 
 }
--- server/src/api/api.service.ts
+++ server/src/api/api.service.ts
@@ -1,18 +1,57 @@
 import axios from 'axios';
 import { Injectable } from '@nestjs/common';
 
+const API_URL = 'https://api.deezer.com';
+
 @Injectable()
 export class ApiService {
   getChart(): Promise<any> {
-    return axios.get('https://api.deezer.com/chart');
+    return axios.get(`${API_URL}/chart`).then(res => ({
+      albums: res.data.albums.data.map(album => ({
+        id: album.id,
+        name: album.name,
+        picture: album.picture_big,
+      })),
+      artists: res.data.artists.data.map(artist => ({
+        id: artist.id,
+        name: artist.name,
+      })),
+      playlists: res.data.playlists.data.map(playlist => ({
+        id: playlist.id,
+        name: playlist.name,
+      })),
+      tracks: res.data.tracks.data.map(track => ({
+        id: track.id,
+        name: track.name,
+        src: track.preview,
+      })),
+    }));
   }
 
-  getPlaylist(id: number) {
-    return axios.get(`https://api.deezer.com/playlist/${id}`);
+  getPlaylist(id: number = 1963962142) {
+    return axios.get(`${API_URL}/playlist/${id}`).then(res => ({
+      tracks: res.data.tracks.data.map(track => ({
+        id: track.id,
+        name: track.title,
+        artist: {
+          id: track.artist.id,
+          name: track.artist.name,
+        },
+        src: track.preview,
+      })),
+    }));
   }
 
   getTrack(id: number) {
-    return axios.get(`https://api.deezer.com/track/${id}`);
+    return axios.get(`${API_URL}/track/${id}`).then(res => ({
+      id: res.data.id,
+      name: res.data.title,
+      artist: {
+        id: res.data.artist.id,
+        name: res.data.artist.name,
+      },
+      src: res.data.preview,
+    }));
   }
 
 }
