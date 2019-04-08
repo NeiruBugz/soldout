d85b0635f38780d84c20525d500aa39ec8b546d2
Dmitry Dergunov <dergunovdv@yandex-team.ru>
2019-04-08 22:40:03 +0300

    backend using nestjs

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,16 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { WsAdapter } from '@nestjs/platform-ws';
 import { AppModule } from './app.module';
-// import * as cors from 'cors';
-//
-// const corsOptions = {
-//   origin: 'http://nestjs.test',
-//   credentials: true,
-// }
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useWebSocketAdapter(new WsAdapter(app));
   app.enableCors();
   await app.listen(3000);
 }
