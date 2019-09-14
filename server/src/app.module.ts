6cebabe525d94001b246b065a71f1724d0ab6498
Dmitry Dergunov <dergunovdv@yandex-team.ru>
2019-09-14 04:26:38 +0300

    add health module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ApiModule } from './modules/api/api.module';
 import { WsModule } from './modules/ws/ws.module';
 import { PlaylistsModule } from './modules/playlists/playlists.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [ApiModule, WsModule, PlaylistsModule],
+  imports: [ApiModule, WsModule, PlaylistsModule, HealthModule],
 })
 export class AppModule {}
