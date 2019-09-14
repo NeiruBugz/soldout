b9e8ac9e5963fd0d9e49ba43fd133add23cbc716
n.badiullin <n.badiullin@transtelematica.ru>
2019-09-15 00:05:27 +0400

    add footer everywhere

--- client/src/layouts/GameField/index.jsx
+++ client/src/layouts/GameField/index.jsx
@@ -1,30 +1,30 @@
 /* eslint-disable no-nested-ternary */
 /* eslint-disable react/no-array-index-key */
-import React from "react";
-import io from "socket.io-client";
-import { connect } from "react-redux";
-import ChoosePlaylist from "../../components/ChoosePlaylist";
+import React from 'react';
+import io from 'socket.io-client';
+import { connect } from 'react-redux';
+import ChoosePlaylist from '../../components/ChoosePlaylist';
 
-import { setTracks } from "../../store/actions/tracks";
-import { setDot } from "../../store/actions/progress";
+import { setTracks } from '../../store/actions/tracks';
+import { setDot } from '../../store/actions/progress';
 
-import AudioComponent from "../../components/AudioComponent";
-import Button from "../../components/Button/Button";
+import AudioComponent from '../../components/AudioComponent';
+import Button from '../../components/Button/Button';
 
-import "./index.scss";
-import ProgressBar from "./ProgressBar";
+import './index.scss';
+import ProgressBar from './ProgressBar';
 
 class GameField extends React.Component {
   constructor(props) {
     super(props);
 
     let url = process.env.REACT_APP_WS_PROD_HOST;
-    if (process.env.NODE_ENV !== "production") {
+    if (process.env.NODE_ENV !== 'production') {
       url = process.env.REACT_APP_WS_DEV_HOST;
     }
 
     this.socket = io(url, {
-      transports: ["websocket"],
+      transports: ['websocket'],
     });
 
     this.state = {
@@ -44,14 +44,14 @@ class GameField extends React.Component {
   putPlayList = () => {
     const { choosedPlaylist: playlistId, setTracks, setDot } = this.props;
 
-    this.socket.emit("start", { playlistId });
-    
-    this.socket.on("tracks", message => {
+    this.socket.emit('start', { playlistId });
+
+    this.socket.on('tracks', message => {
       setTracks(message);
       this.setState({ disabled: false });
     });
 
-    this.socket.on("showCorrect", message => {
+    this.socket.on('showCorrect', message => {
       this.setState({
         choose: message.choose,
         correct: message.correct,
@@ -59,13 +59,13 @@ class GameField extends React.Component {
       });
     });
 
-    this.socket.on("guess", message => {
+    this.socket.on('guess', message => {
       setDot(message);
     });
   };
 
   onChoose = trackId => {
-    this.socket.emit("choose", { trackId });
+    this.socket.emit('choose', { trackId });
   };
 
   render() {
@@ -78,39 +78,50 @@ class GameField extends React.Component {
     const { disabled, choose, correct } = this.state;
 
     return choosedPlaylist ? (
-      <div className="field">
-        <div className="container">
-          <div className="row center-xs">
-            <div className="col-xs">
-              <AudioComponent musicUrl={tracks.src} />
+      <>
+        <div className="field">
+          <div className="container">
+            <div className="row center-xs">
+              <div className="col-xs">
+                <AudioComponent musicUrl={tracks.src} />
+              </div>
+            </div>
+            {isPlaying && (
+              <>
+                <div className="button__grid">
+                  {tracks.tracks.map(item => (
+                    <Button
+                      key={item.id}
+                      id={`track_${item.id}`}
+                      artist={item.artist}
+                      track={item.name}
+                      skin="bright"
+                      status={
+                        item.id === correct
+                          ? 'correct'
+                          : correct && item.id === choose
+                          ? 'error'
+                          : null
+                      }
+                      onClick={() => !disabled && this.onChoose(item.id)}
+                    />
+                  ))}
+                </div>
+                <ProgressBar />
+              </>
+            )}
+          </div>
+        </div>
+        <footer style={{ position: 'fixed', bottom: 40, left: 40 }}>
+          <div className="container">
+            <div className="row">
+              <div className="col-xs">
+                <a href="/form">Дать фидбек</a>
+              </div>
             </div>
           </div>
-          {isPlaying && (
-            <>
-              <div className="button__grid">
-                {tracks.tracks.map(item => (
-                  <Button
-                    key={item.id}
-                    id={`track_${item.id}`}
-                    artist={item.artist}
-                    track={item.name}
-                    skin="bright"
-                    status={
-                      item.id === correct
-                        ? "correct"
-                        : correct && item.id === choose
-                        ? "error"
-                        : null
-                    }
-                    onClick={() => !disabled && this.onChoose(item.id)}
-                  />
-                ))}
-              </div>
-              <ProgressBar />
-            </>
-          )}
-        </div>
-      </div>
+        </footer>
+      </>
     ) : (
       <ChoosePlaylist />
     );
